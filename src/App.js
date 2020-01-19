d96c7c55b0b96969ef7613572e882a38cc5844b1
conbec <56614504+conbec@users.noreply.github.com>
2020-01-19 12:30:02 -0800

    creating events, no date checking

--- src/App.js
+++ src/App.js
@@ -7,23 +7,57 @@ function App() {
   const [date, setDate] = useState(''); //use moment to initialize this
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [events, setEvent] = useState([]);
+  
+
+  const createEvent = () => {
+    const event = { date: date, title: title, content: content }
+    setEvent([...events, event]);
+    setDate('');
+    setTitle('');
+    setContent('');
+  }
+
+
+  // comparetor won't work without some adjustment
+  const deleteEvent = (e) => {
+    events.filter(event => {
+      if (event.title !== e.target.parent) {
+        return event;
+      }
+    });
+  }
+
 
   return (
     <div className="App">
-      <div className="form-panel">
-        <form onSubmit={e => e.preventDefault()}>
-          <input type="text" onChange={e => { setDate(e.target.value) }}></input>
-          <input type="text" placeholder="Title" onChange={e => { setTitle(e.target.value) }}></input>
-          <input type="text" placeholder="Content" onChange={e => { setContent(e.target.value) }}></input>
-          <p>{date}</p>
-          <p>{title}</p>
-          <p>{content}</p>
-          <button>test</button>
-        </form>
-      </div>
-      <div className="display-panel">
-      {/*this will be ul of divs*/}
-      </div>
+      <h2>Acme Event Tracker</h2>
+      <main>
+        <div className="form-panel">
+          <form onSubmit={e => e.preventDefault()}>
+            <input type="text" value={date} placeholder="mm/dd/yyyy" onChange={e => { setDate(e.target.value) }}></input>
+            <input type="text" value={title} placeholder="Title" onChange={e => { setTitle(e.target.value) }}></input>
+            <input type="text" value={content} placeholder="Content" onChange={e => { setContent(e.target.value) }}></input>
+            <button disabled={!date || !title || !content} onClick={() => createEvent()}>Create Event</button>
+          </form>
+        </div>
+        <div className="display-panel">
+          <ul>
+            {events.map((event, idx) => {
+              return (
+                <li key={idx}>
+                  <div className="event-card">
+                    <h4>{event.title}</h4>
+                    <p>{event.date}</p>
+                    <button onClick={(e) => { deleteEvent(e) }}>X</button>
+                    <p>{event.content}</p> 
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+          </div>
+      </main>
     </div>
   );
 }
