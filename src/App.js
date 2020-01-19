bb46b2293c187b032992157e6d6975a31f7d49b7
conbec <56614504+conbec@users.noreply.github.com>
2020-01-19 15:06:37 -0800

    creating events,deleting events, date input error, no date checking

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import './App.css';
 
 
 function App() {
-  const [date, setDate] = useState(''); //use moment to initialize this
+  const [date, setDate] = useState(); //use moment to initialize this
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [events, setEvent] = useState([]);
-  
+
 
   const createEvent = () => {
-    const event = { date: date, title: title, content: content }
+    const event = { date: date, title: title, content: content, key: events.length + 1 };
     setEvent([...events, event]);
     setDate('');
     setTitle('');
@@ -19,13 +19,13 @@ function App() {
   }
 
 
-  // comparetor won't work without some adjustment
-  const deleteEvent = (e) => {
-    events.filter(event => {
-      if (event.title !== e.target.parent) {
+  const deleteEvent = ({ target }) => {
+    const updated = events.filter(event => {
+      if (!target.classList.contains(event.key)) {
         return event;
       }
     });
+    setEvent(updated);
   }
 
 
@@ -38,19 +38,19 @@ function App() {
             <input type="text" value={date} placeholder="mm/dd/yyyy" onChange={e => { setDate(e.target.value) }}></input>
             <input type="text" value={title} placeholder="Title" onChange={e => { setTitle(e.target.value) }}></input>
             <input type="text" value={content} placeholder="Content" onChange={e => { setContent(e.target.value) }}></input>
-            <button disabled={!date || !title || !content} onClick={() => createEvent()}>Create Event</button>
+            <button disabled={!date || !title || !content} onClick={() => createEvent() }>Create Event</button>
           </form>
         </div>
         <div className="display-panel">
           <ul>
             {events.map((event, idx) => {
               return (
-                <li key={idx}>
+                <li key={event.key}>
                   <div className="event-card">
                     <h4>{event.title}</h4>
                     <p>{event.date}</p>
-                    <button onClick={(e) => { deleteEvent(e) }}>X</button>
                     <p>{event.content}</p> 
+                    <button className={event.key} onClick={(e) => { deleteEvent(e) }}>X</button>
                   </div>
                 </li>
               )
